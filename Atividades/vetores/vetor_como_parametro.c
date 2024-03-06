6f0e2574468fc3758ddea64044f85df70dab1033
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-03-06 17:30:31 -0300

    vetor como parametro

---
+++ Atividades/vetores/vetor_como_parametro.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void soma_escalar(int v[], int sz){
+    int escalar = 10;
+    for(int i = 0; i < sz; i++){
+        *(v+i) += escalar;
+    }
+}
+
+void print_vetor(int *v, int sz){
+    for(int i = 0; i < sz; i++){
+        printf("v[%d] = %d | endereço = %p\n", i,*(v+i),(v+i));
+    }
+}
+
+int main(){
+    
+
+    puts("##### VETOR ESTATICO####\n");
+    int v[5] = {0,5,10,15,20};
+    
+    print_vetor(v,5);
+    soma_escalar(v,5);
+    puts("##### SOMANDO A ESCALAR ####\n");
+    print_vetor(v,5);
+
+    int *vh = (int *)calloc(5,sizeof(int));
+    puts("\n##### VETOR ESTATICO####\n");
+    for(int i = 0; i < 5; i++){
+        *(vh+i) = i * 100;
+    }
+    print_vetor(vh,5);
+    puts("##### SOMANDO A ESCALAR ####\n");
+    soma_escalar(vh,5);
+    print_vetor(vh,5);
+    free(vh);
+    return 0;
+}
